9fafb101af94e2dcbccd67b6ffc6d9e8376976f2
justnatalie97 <natawka.ivanova@gmail.com>
2023-05-03 19:03:04 +0300

    data types

---
+++ java/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        //int eto celõe chilsa
+        int number = 10;
+        System.out.println(number);
+        //float double chilsa s plavajusej zapjatoj
+        float number2 =10.5f;
+        System.out.println(number2);
+        double number3 = 10.8;
+        System.out.println(number3);
+        // limit [-128, 128)
+        byte number4 = 127;
+        // short [-32,768 to 32,768)
+        short number5 = 2000;
+        long number6 = 135487435;
+        // char odinochnqj simvol
+        char sym ='*';
+        String str = "hello world!";
+        // boolean true or false
+        boolean isTrue = true;
+        System.out.println(isTrue);
+    }
+}
