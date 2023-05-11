f2fd14fda28e3f24095a76652fb359bd4714e3c1
nataljavonavi <natawka.ivanova@gmail.com>
2023-05-11 19:29:18 +0300

    Homehowk1

---
+++ java/src/Homework2.java
@@ -0,0 +1,7 @@
+public class Homework2 {
+    public static void main(String[] args) {
+        //EVEN 2 4 6 8 10 12 ... 50
+        //ODD 1 3 5 7 9 11 13 ...49
+
+    }
+}
---
+++ java/src/com/company/Main14.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main14{
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5};
+        int num1 = 3;
+        int num2 = 5;
+        System.out.println( Arrays.toString(array));
+        for(int i =0; i< array.length; i++){
+            if(array[i] == num1 || array[i] == num2){
+                array[i] = 50;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
+
+
