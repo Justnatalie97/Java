fcc4febc3d6ca257fd47226bf077b333d6bb087a
nataljavonavi <natawka.ivanova@gmail.com>
2023-05-15 13:55:54 +0300

    HOMEWORK1

--- java/src/com/company/Homework1.java
+++ java/src/com/company/Homework1.java
@@ -5,9 +5,9 @@ public class Homework1 {
         //EVEN 2 4 5 6 8 10 12 ...50
         //ODD 1 3 5 7 9 11 13 ...49
 
-        System.out.print("EVEN" + "\t");
+        System.out.print("EVEN");
             for (int i = 2; i <= 50; i += 2){
-            System.out.print(i + "\t" );
+                System.out.print(i + "\t" );
         }
         System.out.print("\n" + "ODD" + "\t");
             for(int j = 1; j<= 49; j+=2){
