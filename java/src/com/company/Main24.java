5701b252523ce7a6aaf27ea170613fbdbc13310e
nataljavonavi <natawka.ivanova@gmail.com>
2023-05-30 10:08:06 +0300

    homework

--- java/src/com/company/Main24.java
+++ java/src/com/company/Main24.java
@@ -3,14 +3,14 @@ package com.company;
 public class Main24 {
     public static void main(String[] args) {
         int max = 0;
-        int min = ;
+        int min = 0;
         // ne dolzno bqtj ravno nulju
         int[] numbers = {35,6,4,10,651,154683,254,12,241,-15,-1465,-3,-4531};
         for(int i= 0; i < numbers.length; i++){
             if (numbers[i]> max){
                 max = numbers[i];
             }
-            if(numbers[i] == min){
+            if(numbers[i] < min){
                 min = numbers[i];
             }
         }
