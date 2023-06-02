9f324239963bbda1e288b30a987a0bd74a938760
nataljavonavi <natawka.ivanova@gmail.com>
2023-06-02 17:38:08 +0300

    homework

--- java/src/com/company/Homework4.java
+++ java/src/com/company/Homework4.java
@@ -5,16 +5,20 @@ import java.util.Arrays;
 public class Homework4 {
     public static void main(String[] args) {
         String str = "Hello Happy new  year    ";
-        int index = str.indexOf('r');
-        System.out.println(index);
-        //vqvesti index poslednej bukvq
-        //for (int i = 0; i < str.length(); i++) {
-           //if (str.charAt(i) != ' ') {
-                index ++;
-               //System.out.print(i);
+        int length = str.length();
+        int index = length - 1;
+        while (true) {
+            if (str.charAt(index) == ' ') {
+                index--;
+            } else {
+                System.out.println(index);
+                break;
             }
 
         }
-   // }
-//}
+    }
+}
+
+
+
 
